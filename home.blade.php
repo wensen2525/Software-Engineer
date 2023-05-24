ec99ca96252187c846b9f82903a6f4c95be17688
wensen2525 <109505910+wensen2525@users.noreply.github.com>
2023-05-24 10:43:52 +0700

    Initial commit

---
+++ home.blade.php
@@ -0,0 +1,39 @@
+<x-app>
+      {{-- background masukin ke section, set no repeat, set cover --}}
+      <section style="">
+            <div class="container">
+                  <div class="row">
+                        <div class="col">
+                              {{-- gambar --}}gambar
+                        </div>
+                  </div>
+            </div>
+      </section>
+      <section style="">
+            <div class="container">
+                  <div class="row">
+                        <div class="col">
+                              {{-- recomended cosplay --}}recomended
+                        </div>
+                  </div>
+            </div>
+      </section>
+      <section style="">
+            <div class="container">
+                  <div class="row">
+                        <div class="col">
+                              {{-- best selling --}}best 
+                        </div>
+                  </div>
+            </div>
+      </section>
+      <section style="">
+            <div class="container">
+                  <div class="row">
+                        <div class="col">
+                              {{-- popular anime cosplay --}}popular
+                        </div>
+                  </div>
+            </div>
+      </section>
+</x-app>
